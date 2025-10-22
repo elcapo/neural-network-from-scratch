9e796a5736fb77882e22e4b5c2c15f51ea2f4d02
Carlos Capote Pérez-Andreu <carlos.capote@hawara.es>
2025-10-22 18:34:10 +0100

    add methods to compute the confusion matrices

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+import numpy as np
 from nn_from_scratch.dataset import Dataset
 from nn_from_scratch.evaluation import Evaluation
 from nn_from_scratch.network import Network
@@ -14,8 +15,20 @@ def main():
     repository.load(nn, "resources/weights_and_biases.npy")
     evaluation = Evaluation(nn)
 
-    accuracy = evaluation.accuracy(X, Y)
-    print(f"The mean accuracy of the model in the test set is: {accuracy}")
+    predicted_probabilities = nn.forward(X, training=True)
+    Y_pred = np.argmax(predicted_probabilities, axis=0)
+
+    accuracy = evaluation.accuracy(Y, Y_pred)
+    print(f"The mean accuracy of the model in the test set is: {accuracy}\n")
+
+    confusion_matrix = evaluation.confusion_matrix(Y, Y_pred)
+
+    print("The confusion matrix of the model is:\n")
+    print(confusion_matrix)
+    print("\n")
+
+    print("The binary confusion matrix of the model is:\n")
+    print(evaluation.binary_confusion_matrix(Y, Y_pred))
 
 if __name__ == "__main__":
     main()
--- nn_from_scratch/evaluation.py
+++ nn_from_scratch/evaluation.py
@@ -5,10 +5,42 @@ class Evaluation:
     def __init__(self, network: Network):
         self.network = network
 
-    def accuracy(self, X: np.ndarray, Y: np.ndarray) -> float:
-        Y_pred = self.network.forward(X, training=True)
+    def accuracy(self, Y: np.ndarray, Y_pred: np.ndarray) -> float:
+        return round(np.sum(Y == Y_pred) / Y.size, 2)
 
-        predictions = np.argmax(Y_pred, axis=0)
-        accuracy = round(np.sum(predictions == Y) / Y.size, 2)
+    def confusion_matrix(self, Y: np.ndarray, Y_pred: np.ndarray) -> np.ndarray:
+        empty_predictions = {}
+        for predicted_value in range(0, 10):
+            empty_predictions[predicted_value] = 0
 
-        return accuracy
+        prediction_count = {}
+        for real_value in range(0, 10):
+            prediction_count[real_value] = empty_predictions.copy()
+
+        for index, real_value in enumerate(Y):
+            predicted_value = Y_pred[index]
+            prediction_count[real_value][predicted_value] += 1
+
+        confusion_matrix = []
+        for category in prediction_count.values():
+            confusion_matrix.append(list(category.values()))
+
+        return np.array(confusion_matrix)
+
+    def binary_confusion_matrix_per_category(self, Y: np.ndarray, Y_pred: np.ndarray, category: int) -> np.ndarray:
+        confusion_matrix = self.confusion_matrix(Y, Y_pred)
+
+        TP = confusion_matrix[category][category]
+        FN = np.sum(confusion_matrix[category]) - TP
+        FP = np.sum(confusion_matrix[:, category]) - TP
+        TN = np.sum(confusion_matrix) - (TP + FN + FP)
+
+        return np.array([[TP, FN], [FP, TN]])
+
+    def binary_confusion_matrix(self, Y: np.ndarray, Y_pred: np.ndarray) -> np.ndarray:
+        binary_confusion_matrix = np.zeros((2, 2))
+
+        for category in range(0, 10):
+            binary_confusion_matrix += self.binary_confusion_matrix_per_category(Y, Y_pred, category)
+        
+        return binary_confusion_matrix
